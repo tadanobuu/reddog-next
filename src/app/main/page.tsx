4fc2007292de1bab25e8f5f468178601ad901423
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-10-04 17:59:06 +0900

    edit main/page.tsx

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -14,6 +14,7 @@ import Guidance from './components/guidance/Guidance'
 import GameDetail from './components/gameDetail/gameDetail'
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
+import { Card, CardContent } from "@/components/ui/card"
 
 const Main = () => {
 
@@ -44,18 +45,20 @@ const Main = () => {
                             </div>
                             :
                             <>
-                            <div className="bg-green-800 text-white">
-                                <div className="p-6 flex flex-col items-center">
-                                    <div className="space-y-1 mb-4 text-center">
-                                        <Message1 />
-                                        <Message4 />
-                                        <Message2 />
-                                        <Message3 />
-                                    </div>
-                                    <Cards />
-                                    <BetPointDisp />
+                            <Card className="bg-green-800 text-white pt-12 pb-6 px-4 sm:px-6 mt-12 relative overflow-hidden">
+                                <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-700 to-green-900"></div>
+                                <CardContent className="relative z-10">
+                                <div className="space-y-1 mb-4 text-center">
+                                    <Message1 />
+                                    <Message4 />
+                                    <Message2 />
+                                    <Message3 />
                                 </div>
-                            </div>
+
+                                <Cards />
+                                <BetPointDisp />
+                                </CardContent>
+                            </Card>
                             <Buttons /> 
                             </>
                         }
