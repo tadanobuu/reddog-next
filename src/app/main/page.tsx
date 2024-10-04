f540f038f48afabc3a4088150cf019acc078d060
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-10-04 10:49:26 +0900

    main/page.tsxをredux Providerでラッピング

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -3,15 +3,17 @@
 import { useState } from 'react'
 import Header from './components/header/Header'
 import Character from './components/character/Character'
-import Message1 from './components/Message1'
-import Cards from './components/Cards'
-import Message2 from './components/Message2'
+import Message1 from './components/messageComps/Message1'
+import Cards from './components/cards/Cards'
+import Message2 from './components/messageComps/Message2'
 import Buttons from './components/buttons/Buttons'
-import BetPointDisp from './components/betPointDisp'
-import Message3 from './components/Message3'
-import Message4 from './components/Message4'
+import BetPointDisp from './components/betPointDisp/betPointDisp'
+import Message3 from './components/messageComps/Message3'
+import Message4 from './components/messageComps/Message4'
 import Guidance from './components/guidance/Guidance'
 import GameDetail from './components/gameDetail/gameDetail'
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
 
 const Main = () => {
 
@@ -24,37 +26,39 @@ const Main = () => {
     }
 
     return (
-        <div className='w-full grid md:grid-cols-2 gap-8'>
-            <div>
-                <Header />
-                <Character />
-                {!noGuidance ? 
-                    <div className='bg-table-color border-4 border-black' style={{"borderRadius": "60px"}}>
-                        <Guidance />
-                        <div className='clearGuidance'>
-                            <button className='clearButton' onClick={() => clearButtonHandler()}>
-                                ゲームを始める
-                            </button>
+        <Provider store={store}>
+            <div className='w-full grid md:grid-cols-2 gap-8'>
+                <div>
+                    <Header />
+                    <Character />
+                    {!noGuidance ? 
+                        <div className='bg-table-color border-4 border-black' style={{"borderRadius": "60px"}}>
+                            <Guidance />
+                            <div className='clearGuidance'>
+                                <button className='clearButton' onClick={() => clearButtonHandler()}>
+                                    ゲームを始める
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    :
-                    <>
-                    <div className='bg-table-color border-4 border-black' style={{"borderRadius": "60px"}}>
-                        <Message1 />
-                        <Message4 />
-                        <Cards />
-                        <Message2 />
-                        <Message3 />
-                        <BetPointDisp />
-                    </div>
-                    <Buttons /> 
-                    </>
-                }
+                        :
+                        <>
+                        <div className='bg-table-color border-4 border-black' style={{"borderRadius": "60px"}}>
+                            <Message1 />
+                            <Message4 />
+                            <Cards />
+                            <Message2 />
+                            <Message3 />
+                            <BetPointDisp />
+                        </div>
+                        <Buttons /> 
+                        </>
+                    }
+                </div>
+                <div className='hidden md:block mt-10'>
+                    <GameDetail />
+                </div>
             </div>
-            <div className='hidden md:block mt-10'>
-                <GameDetail />
-            </div>
-        </div>
+        </Provider>
     )
 }
 
