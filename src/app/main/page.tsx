84139ee3dd1f3a60baf921a75795f60444a99944
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-10-05 22:16:24 +0900

    カードサイズ調整

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -20,24 +20,24 @@ const Main = () => {
         <Provider store={store}>
             <div className='min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-white'>
                 <Header />
-                <main className="container mx-auto px-4 py-8">
+                <main className="container mx-auto px-0 sm:px-4 py-8">
                     <div className="grid md:grid-cols-2 gap-8">
                         <div className="relative">
                         <Character />
                         <Card className="bg-green-800 text-white pt-12 pb-6 px-4 sm:px-6 mt-12 relative overflow-hidden">
                         <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-700 to-green-900"></div>
-                        <CardContent className="relative z-10">
-                        <div className="space-y-1 mb-4 text-center">
-                            <Message1 />
-                            <Message4 />
-                            <Message2 />
-                            <Message3 />
-                        </div>
-                        <Cards />
-                        <BetPointDisp />
-                        </CardContent>
-                        </Card>
-                        <Buttons /> 
+                            <CardContent className="relative z-10">
+                            <div className="space-y-1 mb-4 text-center">
+                                <Message1 />
+                                <Message4 />
+                                <Message2 />
+                                <Message3 />
+                            </div>
+                            <Cards />
+                            <BetPointDisp />
+                            </CardContent>
+                            </Card>
+                            <Buttons /> 
                         </div>
                         <GameDetail />
                     </div>
